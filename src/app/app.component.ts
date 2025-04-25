6f1446059be3bd98ba476162f097195d8f0b85e8
AdrianSilvadoNascimento <adrianfsf10@gmail.com>
2025-04-25 18:18:16 -0300

    include lucide and tailwind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { LucideAngularModule, Home, User } from 'lucide-angular';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LucideAngularModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'egest_estoque_web';
+  readonly HomeIcon = Home;
+  readonly UserIcon = User;
 }
