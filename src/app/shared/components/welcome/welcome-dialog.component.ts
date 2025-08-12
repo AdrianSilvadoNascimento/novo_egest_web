4c2e763af0ffa72112cc23deb94c96e09d71e430
Adrian Silva <adrianfsf10@gmail.com>
2025-08-11 23:35:51 -0300

    chore: refact greetings modal

--- src/app/shared/components/welcome/welcome-dialog.component.ts
+++ src/app/shared/components/welcome/welcome-dialog.component.ts
@@ -1,19 +1,62 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
 
 import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { MatRippleModule } from '@angular/material/core';
+import { MatIcon } from "@angular/material/icon";
+import { Package, LucideAngularModule, ArrowRight, ArrowLeft, Heart, Settings, TrendingUp, Users, CheckCircle, Star } from 'lucide-angular';
 
 @Component({
   standalone: true,
-  imports: [MatButtonModule, MatDialogModule, MatRippleModule, RouterLink],
+  imports: [MatButtonModule, MatDialogModule, MatRippleModule, MatIcon, LucideAngularModule],
   selector: 'app-welcome-dialog',
   templateUrl: './welcome-dialog.component.html',
   styleUrls: ['./welcome-dialog.component.scss']
 })
 export class WelcomeDialogComponent {
+  readonly packageIcon = Package;
+  readonly arrowLeftIcon = ArrowLeft;
+  readonly arrowRightIcon = ArrowRight
+  readonly heartIcon = Heart
+  readonly usersIcon = Users;
+  readonly trendingUpIcon = TrendingUp;
+  readonly settingsIcon = Settings;
+  readonly checkCircleIcon = CheckCircle;
+  readonly starIcon = Star;
+
+  currentStep: number = 1;
+
+  welcomeSteps = [
+    {
+      title: 'Seja muito bem-vindo(a)! 🎉',
+      description: 'Que alegria ter você aqui conosco!'
+    },
+    {
+      title: 'Sua plataforma, suas regras! ⚡',
+      description: 'Tudo foi pensado para facilitar sua vida'
+    },
+    {
+      title: 'Vamos configurar sua conta? 🛠️',
+      description: 'Só mais alguns detalhes para deixar tudo perfeito!'
+    }
+  ]
+
   constructor(
-    public dialogRef: MatDialogRef<WelcomeDialogComponent>
+    public dialogRef: MatDialogRef<WelcomeDialogComponent>,
+    private router: Router
   ) {}
+
+  previousStep(): void {
+    this.currentStep--;
+  }
+
+  nextStep(): void {
+    this.currentStep++;
+  }
+
+  navigateToSettings(): void {
+    this.router.navigate(['/settings/account']);
+    this.dialogRef.close();
+  }
 } 
